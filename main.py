677663ec573363bbde409f51e4e9e547649e9ede
Bartłomiej Mumot <mumot1999@gmail.com>
2020-11-29 19:20:03 +0100

    refactor: main

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import coin_detector
 if __name__ == "__main__":
     # image = cv2.imread("resources/new/image4.jpg")
     # image = cv2.imread("resources/new/image3.jpg")
-    # image = cv2.imread("resources/new/893.png")
-    image = cv2.imread("resources/new/image2.jpg")
+    image = cv2.imread("resources/new/893.png")
+    # image = cv2.imread("resources/new/image2.jpg")
 
     extractor = coin_detector.CoinExtractor(image)
 
     detector = coin_detector.Detector()
 
-    for i, photo in enumerate(filter(lambda x: x.coin_type.value == 500, detector.samples)):
-        cv2.imshow(str(i), photo.image)
-        for id, debug_image in enumerate(photo.debug_images):
-            cv2.imshow(f"{i}-{id}-debug", debug_image)
+    # for i, photo in enumerate(filter(lambda x: x.coin_type.value == 500, detector.samples)):
+    #     cv2.imshow(str(i), photo.image)
+    #     for id, debug_image in enumerate(photo.debug_images):
+    #         cv2.imshow(f"{i}-{id}-debug", debug_image)
 
     print(detector.score)
 
